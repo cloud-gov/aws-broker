ba488cf95cad1525affd2710335a1e3be83266ca
Carlo Costino <carlo.costino@gsa.gov>
2020-07-16 18:51:24 -0400

    Renamed DB update smoke tests to avoid name collisions

--- ci/run-smoke-tests-db-updates.sh
+++ ci/run-smoke-tests-db-updates.sh
@@ -19,28 +19,28 @@ wait_for_service_instance() {
 cf login -a "$CF_API_URL" -u "$CF_USERNAME" -p "$CF_PASSWORD" -o "$CF_ORGANIZATION" -s "$CF_SPACE"
 
 # Clean up existing app and service if present
-cf delete -f "smoke-tests-$SERVICE_PLAN"
-cf delete-service -f "rds-smoke-tests-$SERVICE_PLAN"
+cf delete -f "smoke-tests-db-update-$SERVICE_PLAN"
+cf delete-service -f "rds-smoke-tests-db-update-$SERVICE_PLAN"
 
 # change into the directory and push the app without starting it.
 pushd aws-db-test/databases/aws-rds
-cf push "smoke-tests-${SERVICE_PLAN}" -f manifest.yml --no-start
+cf push "smoke-tests-db-update-${SERVICE_PLAN}" -f manifest.yml --no-start
 
 # set some variables that it needs
-cf set-env "smoke-tests-${SERVICE_PLAN}" DB_TYPE "${SERVICE_PLAN}"
-cf set-env "smoke-tests-${SERVICE_PLAN}" SERVICE_NAME "rds-smoke-tests-$SERVICE_PLAN"
+cf set-env "smoke-tests-db-update-${SERVICE_PLAN}" DB_TYPE "${SERVICE_PLAN}"
+cf set-env "smoke-tests-db-update-${SERVICE_PLAN}" SERVICE_NAME "rds-smoke-tests-db-update-$SERVICE_PLAN"
 
 # Create service
 if echo "$SERVICE_PLAN" | grep -v shared | grep mysql >/dev/null ; then
   # test out the enable_functions stuff, which only works on non-shared mysql databases
-  cf create-service aws-rds "$SERVICE_PLAN" "rds-smoke-tests-$SERVICE_PLAN" -c '{"enable_functions": true}'
+  cf create-service aws-rds "$SERVICE_PLAN" "rds-smoke-tests-db-update-$SERVICE_PLAN" -c '{"enable_functions": true}'
 else
   # create a regular instance
-  cf create-service aws-rds "$SERVICE_PLAN" "rds-smoke-tests-$SERVICE_PLAN"
+  cf create-service aws-rds "$SERVICE_PLAN" "rds-smoke-tests-db-update-$SERVICE_PLAN"
 fi
 
 while true; do
-  if out=$(cf bind-service "smoke-tests-${SERVICE_PLAN}" "rds-smoke-tests-$SERVICE_PLAN"); then
+  if out=$(cf bind-service "smoke-tests-db-update-${SERVICE_PLAN}" "rds-smoke-tests-db-update-$SERVICE_PLAN"); then
     break
   fi
   if [[ $out =~ "Instance not available yet" ]]; then
@@ -50,14 +50,14 @@ while true; do
 done
 
 # wait for the app to start. if the app starts, it's passed the smoke test.
-cf push "smoke-tests-${SERVICE_PLAN}"
+cf push "smoke-tests-db-update-${SERVICE_PLAN}"
 
 # Update service
-cf update-service "rds-smoke-tests-$SERVICE_PLAN" "$NEW_SERVICE_PLAN"
+cf update-service "rds-smoke-tests-db-update-$SERVICE_PLAN" "$NEW_SERVICE_PLAN"
 
 # Wait to make sure that the service instance has been successfully updated.
-wait_for_service_instance "rds-smoke-tests-$SERVICE_PLAN"
+wait_for_service_instance "rds-smoke-tests-db-update-$SERVICE_PLAN"
 
 # Clean up app and service
-cf delete -f "smoke-tests-$SERVICE_PLAN"
-cf delete-service -f "rds-smoke-tests-$SERVICE_PLAN"
+cf delete -f "smoke-tests-db-update-$SERVICE_PLAN"
+cf delete-service -f "rds-smoke-tests-db-update-$SERVICE_PLAN"
