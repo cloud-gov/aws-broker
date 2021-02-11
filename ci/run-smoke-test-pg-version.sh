c40366b35bd1af604252b934b9826b08d0b56ba1
Carlo Costino <carlo.costino@gsa.gov>
2021-02-11 13:04:13 -0500

    Changed the app and services names to be unique for the PG version smoke tests

--- ci/run-smoke-test-pg-version.sh
+++ ci/run-smoke-test-pg-version.sh
@@ -6,22 +6,22 @@ set -euxo pipefail
 cf login -a "$CF_API_URL" -u "$CF_USERNAME" -p "$CF_PASSWORD" -o "$CF_ORGANIZATION" -s "$CF_SPACE"
 
 # Clean up existing app and service if present
-cf delete -f "smoke-tests-$SERVICE_PLAN"
-cf delete-service -f "rds-smoke-tests-$SERVICE_PLAN"
+cf delete -f "smoke-tests-pg-version-$SERVICE_PLAN"
+cf delete-service -f "rds-smoke-tests-pg-version-$SERVICE_PLAN"
 
 # change into the directory and push the app without starting it.
 pushd aws-db-test/databases/aws-rds
-cf push "smoke-tests-${SERVICE_PLAN}" -f manifest.yml --no-start
+cf push "smoke-tests-pg-version-${SERVICE_PLAN}" -f manifest.yml --no-start
 
 # set some variables that it needs
-cf set-env "smoke-tests-${SERVICE_PLAN}" DB_TYPE "${SERVICE_PLAN}"
-cf set-env "smoke-tests-${SERVICE_PLAN}" SERVICE_NAME "rds-smoke-tests-$SERVICE_PLAN"
+cf set-env "smoke-tests-pg-version-${SERVICE_PLAN}" DB_TYPE "${SERVICE_PLAN}"
+cf set-env "smoke-tests-pg-version-${SERVICE_PLAN}" SERVICE_NAME "rds-smoke-tests-pg-version-$SERVICE_PLAN"
 
 # Create service
-cf create-service aws-rds "$SERVICE_PLAN" "rds-smoke-tests-$SERVICE_PLAN" -c '{"version": 10}'
+cf create-service aws-rds "$SERVICE_PLAN" "rds-smoke-tests-pg-version-$SERVICE_PLAN" -c '{"version": 10}'
 
 while true; do
-  if out=$(cf bind-service "smoke-tests-${SERVICE_PLAN}" "rds-smoke-tests-$SERVICE_PLAN"); then
+  if out=$(cf bind-service "smoke-tests-pg-version-${SERVICE_PLAN}" "rds-smoke-tests-pg-version-$SERVICE_PLAN"); then
     break
   fi
   if [[ $out =~ "Instance not available yet" ]]; then
@@ -31,8 +31,8 @@ while true; do
 done
 
 # wait for the app to start. if the app starts, it's passed the smoke test.
-cf push "smoke-tests-${SERVICE_PLAN}"
+cf push "smoke-tests-pg-version-${SERVICE_PLAN}"
 
 # Clean up app and service
-cf delete -f "smoke-tests-$SERVICE_PLAN"
-cf delete-service -f "rds-smoke-tests-$SERVICE_PLAN"
+cf delete -f "smoke-tests-pg-version-$SERVICE_PLAN"
+cf delete-service -f "rds-smoke-tests-pg-version-$SERVICE_PLAN"
