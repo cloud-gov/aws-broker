52c3157c213b73c72cde35317d37248c4b05526f
Van Nguyen <vanh.nguyen@gsa.gov>
2021-04-13 15:23:40 -0400

    Added outputs to base terraform folder

---
+++ ci/terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "rds_internal" {
+  value = module.rds_internal
+}
+
+output "rds_shared_mysql" {
+  value = module.rds_shared_mysql
+}
+
+output "rds_shared_postgres" {
+  value = module.rds_shared_postgres
+}
