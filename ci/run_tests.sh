83898e7e310736411c8ea1322251e9dae8f533eb
Ben Berry <ben.berry@gsa.gov>
2021-10-01 12:53:53 -0700

    update tests to match local dev, add notes on local dev

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -2,16 +2,7 @@
 
 set -e -x
 
-export GOPATH=$(pwd)/gopath
-export PATH=$PATH:$GOPATH/bin
-
-go get github.com/tools/godep
-
-cd gopath/src/github.com/18F/aws-broker
-
 cp secrets-test.yml secrets.yml
 cp catalog-test.yml catalog.yml
 
-godep get
-godep go build ./...
-godep go test ./...
+go test
