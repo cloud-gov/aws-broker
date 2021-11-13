6e7938db7d91c3e753ccce3c268cc92beb5894a3
folksgl <Gfolks14@gmail.com>
2021-11-13 15:24:53 -0500

    Refactor error checking logic to be idiomatic go

--- db/db.go
+++ db/db.go
@@ -15,13 +15,14 @@ import (
 // In addition to calling DBInit(), it also makes sure that the tables are setup for Instance and DBConfig structs.
 func InternalDBInit(dbConfig *common.DBConfig) (*gorm.DB, error) {
 	db, err := common.DBInit(dbConfig)
-	if err == nil {
-		db.DB().SetMaxOpenConns(10)
-		log.Println("Migrating")
-		// db.LogMode(true)
-		// Automigrate!
-		db.AutoMigrate(&rds.RDSInstance{}, &redis.RedisInstance{}, &elasticsearch.ElasticsearchInstance{}, &base.Instance{}) // Add all your models here to help setup the database tables
-		log.Println("Migrated")
+	if err != nil {
+		panic(err)
 	}
+	db.DB().SetMaxOpenConns(10)
+	log.Println("Migrating")
+	// db.LogMode(true)
+	// Automigrate!
+	db.AutoMigrate(&rds.RDSInstance{}, &redis.RedisInstance{}, &elasticsearch.ElasticsearchInstance{}, &base.Instance{}) // Add all your models here to help setup the database tables
+	log.Println("Migrated")
 	return db, err
 }
