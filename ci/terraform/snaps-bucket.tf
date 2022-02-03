de92be682b32a45a606b492c6be7eea4886f5a4e
Rian Bogle <rian.bogle@gsa.gov>
2022-02-03 09:44:52 -0700

    s3 permissions

--- ci/terraform/snaps-bucket.tf
+++ ci/terraform/snaps-bucket.tf
@@ -1,5 +1,7 @@
 module "snapshot_bucket"{
     source = "./s3_module"
     stack_description = var.stack_description
-    s3_bucket_prefix = var.s3_snapshots_bucket_name_prefix
+    s3_bucket_suffix = "-aws-broker-snapshot-storage"
+    base_stack = var.base_stack
+    access_role_arn = var.platform_access_role_arn
 }
