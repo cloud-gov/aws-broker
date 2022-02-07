8b35c8ac113909c62a367f86627dda4f2b87b95e
Rian Bogle <rian.bogle@gsa.gov>
2022-02-07 14:53:17 -0700

    reduce expiration in dev and stage

--- ci/terraform/snaps-bucket.tf
+++ ci/terraform/snaps-bucket.tf
@@ -4,4 +4,5 @@ module "snapshot_bucket"{
     s3_bucket_suffix = "aws-broker-snapshot-storage"
     base_stack = var.base_stack
     access_role_arn = var.platform_access_role_arn
+    s3_expire_after_days = var.snapshot_expiration
 }
