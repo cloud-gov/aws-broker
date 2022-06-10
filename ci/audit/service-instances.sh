821b17317876d663c8f383830f07db17b9c8945e
Van Nguyen <vanh.nguyen@gsa.gov>
2022-06-10 14:33:57 -0400

    csv instead of text and monthly trigger

--- ci/audit/service-instances.sh
+++ ci/audit/service-instances.sh
@@ -4,7 +4,7 @@ function main() {
   cf api "${CF_API_URL}"
   (set +x; cf auth "${CF_USERNAME}" "${CF_PASSWORD}")
 
-  cg-scripts/audit/cf-service-instances-by-service-offering.sh ${CF_SERVICE} >> email-out/${CF_SERVICE}.txt
+  cg-scripts/audit/cf-service-instances-by-service-offering.sh ${CF_SERVICE} >> email-out/${CF_SERVICE}.csv
 }
 
 main
