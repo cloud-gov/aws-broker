c951af52e459a192099551bbd40cbe561076617b
Mark Boyd <mark.boyd@gsa.gov>
2023-06-29 16:52:49 -0400

    remove unnecessary interface

--- awsiam/user.go
+++ awsiam/user.go
@@ -4,18 +4,6 @@ import (
 	"errors"
 )
 
-type User interface {
-	Describe(userName string) (UserDetails, error)
-	Create(userName, iamPath string) (string, error)
-	Delete(userName string) error
-	ListAccessKeys(userName string) ([]string, error)
-	CreateAccessKey(userName string) (string, string, error)
-	DeleteAccessKey(userName, accessKeyID string) error
-	ListAttachedUserPolicies(userName, iamPath string) ([]string, error)
-	AttachUserPolicy(userName, policyARN string) error
-	DetachUserPolicy(userName, policyARN string) error
-}
-
 type UserDetails struct {
 	UserName string
 	UserARN  string
