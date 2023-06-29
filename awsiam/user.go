383dcca55e7de189cbe4fb577ce8e38146dd18e0
Mark Boyd <mark.boyd@gsa.gov>
2023-06-29 16:53:25 -0400

    remove unused error

--- awsiam/user.go
+++ awsiam/user.go
@@ -1,15 +1,7 @@
 package awsiam
 
-import (
-	"errors"
-)
-
 type UserDetails struct {
 	UserName string
 	UserARN  string
 	UserID   string
 }
-
-var (
-	ErrUserDoesNotExist = errors.New("iam user does not exist")
-)
