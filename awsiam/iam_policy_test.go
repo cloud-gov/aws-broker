c85c97f0bf76502f39ebec8fee8ca897980f0477
Mark Boyd <mark.boyd@gsa.gov>
2023-06-30 09:40:44 -0400

    clean up test

--- awsiam/iam_policy_test.go
+++ awsiam/iam_policy_test.go
@@ -545,52 +545,6 @@ func TestCreatePolicyFromTemplate(t *testing.T) {
 			iamPath:            "/path/",
 			expectedErrMessage: "code: message",
 		},
-		// "returns error": {
-		// 	policyHandler: &IamPolicyHandler{
-		// 		iamsvc: &mockIamClient{
-		// 			createPolicyErr: errors.New("create policy error"),
-		// 		},
-		// 		logger: logger,
-		// 	},
-		// 	policyName: "policy-name",
-		// 	policyTemplate: `{
-		// 		"Version": "2012-10-17",
-		// 		"Id": "policy-name",
-		// 		"Statement": [
-		// 			{
-		// 				"Effect": "effect",
-		// 				"Action": "action",
-		// 				"Resource": {{resources "/*"}}
-		// 			}
-		// 		]
-		// 	}`,
-		// 	resources:          []string{"resource"},
-		// 	iamPath:            "/path/",
-		// 	expectedErrMessage: "create policy error",
-		// },
-		// "returns AWS error": {
-		// 	policyHandler: &IamPolicyHandler{
-		// 		iamsvc: &mockIamClient{
-		// 			createPolicyErr: awserr.New("code", "message", errors.New("operation failed")),
-		// 		},
-		// 		logger: logger,
-		// 	},
-		// 	policyName: "policy-name",
-		// 	policyTemplate: `{
-		// 		"Version": "2012-10-17",
-		// 		"Id": "policy-name",
-		// 		"Statement": [
-		// 			{
-		// 				"Effect": "effect",
-		// 				"Action": "action",
-		// 				"Resource": {{resources "/*"}}
-		// 			}
-		// 		]
-		// 	}`,
-		// 	resources:          []string{"resource"},
-		// 	iamPath:            "/path/",
-		// 	expectedErrMessage: "code: message",
-		// },
 	}
 
 	for name, test := range testCases {
