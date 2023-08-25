bff67807776babfb51015697f0e0882fdc6dfe8b
Ephraim-G <ephraim.gross@gsa.gov>
2023-08-25 15:48:45 -0400

    fixed unit tests for catalog to use valid version number

--- main_test.go
+++ main_test.go
@@ -44,7 +44,7 @@ var createRDSPGWithVersionInstanceReq = []byte(
 	"service_id":"db80ca29-2d1b-4fbc-aad3-d03c0bfa7593",
 	"plan_id":"da91e15c-98c9-46a9-b114-02b8d28062c6",
 	"parameters": {
-		"version": "10"
+		"version": "15"
 	},
 	"organization_guid":"an-org",
 	"space_guid":"a-space"
