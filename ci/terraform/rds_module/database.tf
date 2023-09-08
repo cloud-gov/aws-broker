e38e31f599af6533215437a57ad11dd7f70c2431
Mark Boyd <mark.boyd@gsa.gov>
2023-09-08 12:28:52 -0400

    Fix RDS database deployments (#332)
    * update internal databases to version 12.14
    
    * update Terraform provider to < 6.0.0
    
    * fix terraform output reference
    
    * change database engine version back to 12
    
    * make iops property conditional

--- ci/terraform/rds_module/database.tf
+++ ci/terraform/rds_module/database.tf
@@ -17,10 +17,10 @@ resource "aws_db_instance" "rds_database" {
 
   auto_minor_version_upgrade = true
 
-  name              = var.rds_db_name
+  db_name              = var.rds_db_name
   allocated_storage = var.rds_db_size
   storage_type      = var.rds_db_storage_type
-  iops              = var.rds_db_iops
+  iops              = var.rds_db_size < 400 && var.rds_db_engine == "postgres" ? null : var.rds_db_iops
   instance_class    = var.rds_instance_type
 
   username = var.rds_username
