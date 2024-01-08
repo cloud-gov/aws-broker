e4c629a06bbee58061573c8903859a98e5a52b9e
Mark Boyd <mark.boyd@gsa.gov>
2024-01-08 16:12:40 -0500

    add tag manager to redis broker

--- services/elasticsearch/broker.go
+++ services/elasticsearch/broker.go
@@ -51,44 +51,15 @@ type elasticsearchBroker struct {
 	tagManager brokertags.TagGenerator
 }
 
-type mockTagGenerator struct {
-	tags map[string]string
-}
-
-func (mt *mockTagGenerator) GenerateTags(
-	action brokertags.Action,
-	serviceGUID string,
-	servicePlanGUID string,
-	organizationGUID string,
-	spaceGUID string,
-	instanceGUID string,
-) (map[string]string, error) {
-	return mt.tags, nil
-}
-
 // InitelasticsearchBroker is the constructor for the elasticsearchBroker.
-func InitElasticsearchBroker(brokerDB *gorm.DB, settings *config.Settings, taskqueue *taskqueue.QueueManager) (base.Broker, error) {
+func InitElasticsearchBroker(
+	brokerDB *gorm.DB,
+	settings *config.Settings,
+	taskqueue *taskqueue.QueueManager,
+	tagManager brokertags.TagGenerator,
+) (base.Broker, error) {
 	logger := lager.NewLogger("aws-es-broker")
 	logger.RegisterSink(lager.NewWriterSink(os.Stdout, lager.INFO))
-
-	if settings.Environment == "test" {
-		return &elasticsearchBroker{
-			brokerDB:   brokerDB,
-			settings:   settings,
-			taskqueue:  taskqueue,
-			logger:     logger,
-			tagManager: &mockTagGenerator{},
-		}, nil
-	}
-
-	tagManager, err := brokertags.NewManager(
-		settings.CfApiUrl,
-		settings.CfApiClientId,
-		settings.CfApiClientSecret,
-	)
-	if err != nil {
-		return nil, err
-	}
 
 	return &elasticsearchBroker{
 		brokerDB,
