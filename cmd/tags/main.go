01a7a1e643d89d008953a5e91135b1b29386f9e0
Mark Boyd <mark.boyd@gsa.gov>
2024-12-16 15:30:59 -0500

    add stub for script to update tags on database instances

---
+++ cmd/tags/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	"github.com/18F/aws-broker/config"
+	"github.com/18F/aws-broker/db"
+	"github.com/18F/aws-broker/services/rds"
+)
+
+func main() {
+	var settings config.Settings
+
+	// Load settings from environment
+	if err := settings.LoadFromEnv(); err != nil {
+		log.Fatalf("There was an error loading settings: %s", err)
+		return
+	}
+
+	db, err := db.InternalDBInit(settings.DbConfig)
+	if err != nil {
+		log.Fatalf("There was an error with the DB. Error: %s", err.Error())
+		return
+	}
+
+	result := db.Find(&rds.RDSInstance{})
+	if result.Error != nil {
+		log.Fatal(result.Error)
+	}
+}
