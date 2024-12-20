30daaae08f50a4bc47b9bd17ae7c6782472fe6a7
Mark Boyd <mark.boyd@gsa.gov>
2024-12-20 15:00:44 -0500

    fix logic for tagging log groups

--- cmd/tasks/rds/tags.go
+++ cmd/tasks/rds/tags.go
@@ -3,6 +3,7 @@ package rds
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/18F/aws-broker/catalog"
 	"github.com/18F/aws-broker/services/rds"
@@ -163,18 +164,21 @@ func ReconcileRDSResourceTags(catalog *catalog.Catalog, db *gorm.DB, rdsClient r
 
 		for _, logGroupType := range rdsInstance.EnabledCloudwatchLogGroupExports {
 			logGroupName := getLogGroupPrefix(rdsInstance.Database, logGroupType)
+			log.Printf("adding tags to log group %s", logGroupName)
+
 			resp, err := logs.DescribeLogGroups(logsClient, logGroupName)
 			if err != nil {
 				return err
 			}
 
-			logGroupArn := resp.LogGroups[0].Arn
+			logGroupArn := *resp.LogGroups[0].Arn
+			logGroupArn, _ = strings.CutSuffix(logGroupArn, ":*")
 
 			cloudwatchTags := make(map[string]*string)
 			for key, value := range generatedTags {
 				cloudwatchTags[key] = aws.String(value)
 			}
-			err = logs.TagCloudwatchLogGroup(logsClient, *logGroupArn, cloudwatchTags)
+			err = logs.TagCloudwatchLogGroup(logsClient, logGroupArn, cloudwatchTags)
 			if err != nil {
 				return err
 			}
