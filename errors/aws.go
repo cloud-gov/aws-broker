5c12f1cbce7d46c1b8ea5b51ee95c9060cccb13d
Mark Boyd <mark.boyd@gsa.gov>
2025-06-04 18:03:42 -0400

    change function name for centralized AWS error logging to LogAWSError

--- errors/aws.go
+++ errors/aws.go
@@ -6,22 +6,16 @@ import (
 	"github.com/aws/aws-sdk-go/aws/awserr"
 )
 
-func DidAwsCallSucceed(err error) bool {
-	// TODO Eventually return a formatted error object.
-	if err != nil {
-		if awsErr, ok := err.(awserr.Error); ok {
-			// Generic AWS Error with Code, Message, and original error (if any)
-			fmt.Println(awsErr.Code(), awsErr.Message(), awsErr.OrigErr())
-			if reqErr, ok := err.(awserr.RequestFailure); ok {
-				// A service error occurred
-				fmt.Println(reqErr.Code(), reqErr.Message(), reqErr.StatusCode(), reqErr.RequestID())
-			}
-		} else {
-			// This case should never be hit, The SDK should alwsy return an
-			// error which satisfies the awserr.Error interface.
-			fmt.Println(err.Error())
+func LogAWSError(err error) {
+	if awsErr, ok := err.(awserr.Error); ok {
+		fmt.Println(awsErr.Code(), awsErr.Message(), awsErr.OrigErr())
+		if reqErr, ok := err.(awserr.RequestFailure); ok {
+			// A service error occurred
+			fmt.Println(reqErr.Code(), reqErr.Message(), reqErr.StatusCode(), reqErr.RequestID())
 		}
-		return false
+	} else {
+		// This case should never be hit, The SDK should alwsy return an
+		// error which satisfies the awserr.Error interface.
+		fmt.Println(err.Error())
 	}
-	return true
 }
