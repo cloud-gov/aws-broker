b9b24c56c63e43b6822f8844d63212b7450ca370
Mark Boyd <mark.boyd@gsa.gov>
2025-06-04 17:28:59 -0400

    add new errors package to contain general error helpers, including DidAwsCallSucceed

---
+++ errors/aws.go
@@ -0,0 +1,27 @@
+package errors
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws/awserr"
+)
+
+func DidAwsCallSucceed(err error) bool {
+	// TODO Eventually return a formatted error object.
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+			// Generic AWS Error with Code, Message, and original error (if any)
+			fmt.Println(awsErr.Code(), awsErr.Message(), awsErr.OrigErr())
+			if reqErr, ok := err.(awserr.RequestFailure); ok {
+				// A service error occurred
+				fmt.Println(reqErr.Code(), reqErr.Message(), reqErr.StatusCode(), reqErr.RequestID())
+			}
+		} else {
+			// This case should never be hit, The SDK should alwsy return an
+			// error which satisfies the awserr.Error interface.
+			fmt.Println(err.Error())
+		}
+		return false
+	}
+	return true
+}
