a02e76b971fb4bc008be0eed8b9017e81d80c11e
Mark Boyd <mark.boyd@gsa.gov>
2025-06-05 11:37:45 -0400

    refactor more code to use LogAWSError() for logging AWS errors

--- services/elasticsearch/elasticsearch.go
+++ services/elasticsearch/elasticsearch.go
@@ -276,18 +276,7 @@ func (d *dedicatedElasticsearchAdapter) checkElasticsearchStatus(i *Elasticsearc
 
 		resp, err := d.opensearch.DescribeDomain(params)
 		if err != nil {
-			if awsErr, ok := err.(awserr.Error); ok {
-				// Generic AWS error with Code, Message, and original error (if any)
-				fmt.Println(awsErr.Code(), awsErr.Message(), awsErr.OrigErr())
-				if reqErr, ok := err.(awserr.RequestFailure); ok {
-					// A service error occurred
-					fmt.Println(reqErr.Code(), reqErr.Message(), reqErr.StatusCode(), reqErr.RequestID())
-				}
-			} else {
-				// This case should never be hit, the SDK should always return an
-				// error which satisfies the awserr.Error interface.
-				fmt.Println(err.Error())
-			}
+			brokerErrs.LogAWSError(err)
 			return base.InstanceNotCreated, err
 		}
 
