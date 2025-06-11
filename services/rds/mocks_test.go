0cd55487c8f0642c2e8a4ba330c1bffab3743704
Mark Boyd <mark.boyd@gsa.gov>
2025-06-11 11:32:57 -0400

    move another mock to share mocks test file for rds package

--- services/rds/mocks_test.go
+++ services/rds/mocks_test.go
@@ -143,3 +143,90 @@ func (m *MockDbUtils) generateDatabaseName(settings *config.Settings) string {
 func (m *MockDbUtils) buildUsername() string {
 	return m.mockUsername
 }
+
+type mockRDSClient struct {
+	rdsiface.RDSAPI
+
+	dbEngineVersions                    []*rds.DBEngineVersion
+	describeEngVersionsErr              error
+	describeDbParamsErr                 error
+	createDbParamGroupErr               error
+	modifyDbParamGroupErr               error
+	describeEngineDefaultParamsResults  []*rds.DescribeEngineDefaultParametersOutput
+	describeEngineDefaultParamsErr      error
+	describeEngineDefaultParamsNumPages int
+	describeEngineDefaultParamsPageNum  int
+	describeDbParamsResults             []*rds.DescribeDBParametersOutput
+	describeDbParamsNumPages            int
+	describeDbParamsPageNum             int
+	describeDbInstancesResults          *rds.DescribeDBInstancesOutput
+	describeDbInstancesErr              error
+}
+
+func (m mockRDSClient) DescribeDBParameters(*rds.DescribeDBParametersInput) (*rds.DescribeDBParametersOutput, error) {
+	if m.describeDbParamsErr != nil {
+		return nil, m.describeDbParamsErr
+	}
+	return nil, nil
+}
+
+func (m mockRDSClient) DescribeDBEngineVersions(*rds.DescribeDBEngineVersionsInput) (*rds.DescribeDBEngineVersionsOutput, error) {
+	if m.describeEngVersionsErr != nil {
+		return nil, m.describeEngVersionsErr
+	}
+	if m.dbEngineVersions != nil {
+		return &rds.DescribeDBEngineVersionsOutput{
+			DBEngineVersions: m.dbEngineVersions,
+		}, nil
+	}
+	return nil, nil
+}
+
+func (m mockRDSClient) CreateDBParameterGroup(*rds.CreateDBParameterGroupInput) (*rds.CreateDBParameterGroupOutput, error) {
+	if m.createDbParamGroupErr != nil {
+		return nil, m.createDbParamGroupErr
+	}
+	return nil, nil
+}
+
+func (m mockRDSClient) ModifyDBParameterGroup(*rds.ModifyDBParameterGroupInput) (*rds.DBParameterGroupNameMessage, error) {
+	if m.modifyDbParamGroupErr != nil {
+		return nil, m.modifyDbParamGroupErr
+	}
+	return nil, nil
+}
+
+func (m *mockRDSClient) DescribeEngineDefaultParametersPages(input *rds.DescribeEngineDefaultParametersInput, fn func(*rds.DescribeEngineDefaultParametersOutput, bool) bool) error {
+	if m.describeEngineDefaultParamsErr != nil {
+		return m.describeEngineDefaultParamsErr
+	}
+	shouldContinue := true
+	for shouldContinue {
+		output := m.describeEngineDefaultParamsResults[m.describeEngineDefaultParamsPageNum]
+		m.describeEngineDefaultParamsPageNum++
+		lastPage := m.describeEngineDefaultParamsPageNum == m.describeEngineDefaultParamsNumPages
+		shouldContinue = fn(output, lastPage)
+	}
+	return nil
+}
+
+func (m *mockRDSClient) DescribeDBParametersPages(input *rds.DescribeDBParametersInput, fn func(*rds.DescribeDBParametersOutput, bool) bool) error {
+	if m.describeDbParamsErr != nil {
+		return m.describeDbParamsErr
+	}
+	shouldContinue := true
+	for shouldContinue {
+		output := m.describeDbParamsResults[m.describeDbParamsPageNum]
+		m.describeDbParamsPageNum++
+		lastPage := m.describeDbParamsPageNum == m.describeDbParamsNumPages
+		shouldContinue = fn(output, lastPage)
+	}
+	return nil
+}
+
+func (m *mockRDSClient) DescribeDBInstances(input *rds.DescribeDBInstancesInput) (*rds.DescribeDBInstancesOutput, error) {
+	if m.describeDbInstancesErr != nil {
+		return nil, m.describeDbInstancesErr
+	}
+	return m.describeDbInstancesResults, nil
+}
