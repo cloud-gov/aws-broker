07f592a7acce28fa8ca49735a15a501bc6ff6789
Mark Boyd <mark.boyd@gsa.gov>
2025-06-17 16:15:20 -0400

    create smoke test for creating elasticsearch instance

--- services/elasticsearch/broker_test.go
+++ services/elasticsearch/broker_test.go
@@ -1,9 +1,18 @@
 package elasticsearch
 
 import (
+	"net/http"
 	"testing"
 
+	"github.com/cloud-gov/aws-broker/base"
+	"github.com/cloud-gov/aws-broker/catalog"
+	"github.com/cloud-gov/aws-broker/common"
 	"github.com/cloud-gov/aws-broker/config"
+	"github.com/cloud-gov/aws-broker/helpers"
+	"github.com/cloud-gov/aws-broker/helpers/request"
+	"github.com/cloud-gov/aws-broker/mocks"
+	"github.com/cloud-gov/aws-broker/taskqueue"
+	"github.com/jinzhu/gorm"
 )
 
 func TestValidate(t *testing.T) {
@@ -36,6 +45,71 @@ func TestValidate(t *testing.T) {
 			}
 			if !test.expectedErr && err != nil {
 				t.Fatalf("unexpected error: %s", err)
+			}
+		})
+	}
+}
+
+func testDBInit() (*gorm.DB, error) {
+	db, err := common.TestDbInit()
+	// Automigrate!
+	db.AutoMigrate(&ElasticsearchInstance{}, &base.Instance{}, &taskqueue.AsyncJobMsg{})
+	return db, err
+}
+
+func TestCreateInstance(t *testing.T) {
+	brokerDB, err := testDBInit()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testCases := map[string]struct {
+		planID               string
+		instance             *ElasticsearchInstance
+		expectedResponseCode int
+		catalog              *catalog.Catalog
+		createRequest        request.Request
+		elasticsearchBroker  *elasticsearchBroker
+	}{
+		"success": {
+			catalog: &catalog.Catalog{
+				ElasticsearchService: catalog.ElasticsearchService{
+					Plans: []catalog.ElasticsearchPlan{
+						{
+							Plan: catalog.Plan{
+								ID: "123",
+							},
+						},
+					},
+				},
+			},
+			planID: "123",
+			instance: &ElasticsearchInstance{
+				Instance: base.Instance{
+					Uuid: helpers.RandStr(10),
+				},
+			},
+			createRequest: request.Request{
+				PlanID: "123",
+			},
+			elasticsearchBroker: &elasticsearchBroker{
+				settings: &config.Settings{
+					EncryptionKey: helpers.RandStr(32),
+					Environment:   "test", // use the mock adapter
+				},
+				tagManager: &mocks.MockTagGenerator{},
+				brokerDB:   brokerDB,
+			},
+			expectedResponseCode: http.StatusAccepted,
+		},
+	}
+
+	for name, test := range testCases {
+		t.Run(name, func(t *testing.T) {
+			response := test.elasticsearchBroker.CreateInstance(test.catalog, test.instance.Uuid, test.createRequest)
+
+			if response.GetStatusCode() != test.expectedResponseCode {
+				t.Errorf("expected: %d, got: %d", test.expectedResponseCode, response.GetStatusCode())
 			}
 		})
 	}
