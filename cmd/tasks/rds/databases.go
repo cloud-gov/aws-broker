55985deb120c28d1c6c0598c7a8a4084abd9e39c
Mark Boyd <mark.boyd@gsa.gov>
2025-09-26 11:26:45 -0400

    add task for finding orphaned RDS instances

---
+++ cmd/tasks/rds/databases.go
@@ -0,0 +1,34 @@
+package rds
+
+import (
+	"errors"
+	"log"
+
+	awsRds "github.com/aws/aws-sdk-go/service/rds"
+	"github.com/aws/aws-sdk-go/service/rds/rdsiface"
+	"github.com/cloud-gov/aws-broker/services/rds"
+	"gorm.io/gorm"
+)
+
+func FindOrphanedInstances(rdsClient rdsiface.RDSAPI, db *gorm.DB) error {
+	err := rdsClient.DescribeDBInstancesPages(&awsRds.DescribeDBInstancesInput{}, func(page *awsRds.DescribeDBInstancesOutput, lastPage bool) bool {
+		for _, dbInstance := range page.DBInstances {
+			var rdsDatabase rds.RDSInstance
+			err := db.Where(&rds.RDSInstance{Database: *dbInstance.DBName}).First(&rdsDatabase).Error
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				log.Printf("database %s does not exist in the broker database", *dbInstance.DBName)
+
+			} else {
+				log.Printf("encountered error trying to fetch record from database: %s", err)
+			}
+			continue
+		}
+		return !lastPage // Continue iterating until the last page is reached.
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
