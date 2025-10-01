14bfd127147c34d5d05e2434fce25fa6492d5f4e
Mark Boyd <mark.boyd@gsa.gov>
2025-10-01 12:33:43 -0400

    improve error handling and add condition to skip database for broker itself (#436)

--- cmd/tasks/rds/databases.go
+++ cmd/tasks/rds/databases.go
@@ -11,20 +11,26 @@ import (
 	"gorm.io/gorm"
 )
 
-func FindOrphanedInstances(rdsClient rdsiface.RDSAPI, db *gorm.DB, dbNamePrefix string) error {
+func FindOrphanedInstances(rdsClient rdsiface.RDSAPI, db *gorm.DB, dbNamePrefix string, dbConfigUrl string) error {
 	err := rdsClient.DescribeDBInstancesPages(&awsRds.DescribeDBInstancesInput{}, func(page *awsRds.DescribeDBInstancesOutput, lastPage bool) bool {
 		for _, dbInstance := range page.DBInstances {
 			instanceName := *dbInstance.DBInstanceIdentifier
 			if !strings.Contains(instanceName, dbNamePrefix) {
-				log.Printf("database %s is not a brokered database, continuing", instanceName)
+				log.Printf("database %s is not a brokered database for this environment, continuing", instanceName)
+				continue
+			}
+			if strings.Contains(dbConfigUrl, instanceName) {
+				log.Printf("database %s is the database for the broker itself, continuing", instanceName)
 				continue
 			}
 			var rdsDatabase rds.RDSInstance
 			err := db.Where(&rds.RDSInstance{Database: instanceName}).First(&rdsDatabase).Error
-			if errors.Is(err, gorm.ErrRecordNotFound) {
-				log.Printf("database %s does not exist in the broker database", instanceName)
-			} else {
-				log.Printf("encountered error trying to fetch record from database: %s", err)
+			if err != nil {
+				if errors.Is(err, gorm.ErrRecordNotFound) {
+					log.Printf("database %s does not exist in the broker database", instanceName)
+				} else {
+					log.Printf("encountered error trying to fetch record from database: %s", err)
+				}
 			}
 			continue
 		}
