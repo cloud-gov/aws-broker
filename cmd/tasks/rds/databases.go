bc603e71c0a6172a0a526c67572ecb37aadc9919
Mark Boyd <mark.boyd@gsa.gov>
2025-10-01 11:48:13 -0400

    fix logic for finding orphaned RDS instances (#435)

--- cmd/tasks/rds/databases.go
+++ cmd/tasks/rds/databases.go
@@ -14,15 +14,15 @@ import (
 func FindOrphanedInstances(rdsClient rdsiface.RDSAPI, db *gorm.DB, dbNamePrefix string) error {
 	err := rdsClient.DescribeDBInstancesPages(&awsRds.DescribeDBInstancesInput{}, func(page *awsRds.DescribeDBInstancesOutput, lastPage bool) bool {
 		for _, dbInstance := range page.DBInstances {
-			dbName := *dbInstance.DBName
-			if !strings.Contains(dbName, dbNamePrefix) {
-				log.Printf("database %s is not a brokered database, continuing", dbName)
+			instanceName := *dbInstance.DBInstanceIdentifier
+			if !strings.Contains(instanceName, dbNamePrefix) {
+				log.Printf("database %s is not a brokered database, continuing", instanceName)
 				continue
 			}
 			var rdsDatabase rds.RDSInstance
-			err := db.Where(&rds.RDSInstance{Database: dbName}).First(&rdsDatabase).Error
+			err := db.Where(&rds.RDSInstance{Database: instanceName}).First(&rdsDatabase).Error
 			if errors.Is(err, gorm.ErrRecordNotFound) {
-				log.Printf("database %s does not exist in the broker database", dbName)
+				log.Printf("database %s does not exist in the broker database", instanceName)
 			} else {
 				log.Printf("encountered error trying to fetch record from database: %s", err)
 			}
