8d0d0b4a056bd346a092e6fb81322a1dfdf44b72
Mark Boyd <mark.boyd@gsa.gov>
2025-10-06 13:56:48 -0400

    update uri connection string in credentials for Redis instances & add test for new behavior

--- services/redis/redisinstance_test.go
+++ services/redis/redisinstance_test.go
@@ -3,6 +3,7 @@ package redis
 import (
 	"testing"
 
+	"github.com/cloud-gov/aws-broker/base"
 	"github.com/cloud-gov/aws-broker/catalog"
 	"github.com/cloud-gov/aws-broker/config"
 	"github.com/cloud-gov/aws-broker/helpers"
@@ -39,6 +40,33 @@ func TestInitInstanceTags(t *testing.T) {
 	}
 
 	if diff := deep.Equal(instance.Tags, expectedTags); diff != nil {
+		t.Error(diff)
+	}
+}
+
+func TestGetCredentials(t *testing.T) {
+	instance := &RedisInstance{
+		Instance: base.Instance{
+			Host: "host",
+			Port: 6379,
+		},
+		EngineVersion: "5",
+	}
+
+	credentials, err := instance.getCredentials("foobar")
+	if err != nil {
+		t.Fatal(err)
+	}
+	expectedCredentials := map[string]string{
+		"uri":                          "rediss://:foobar@host:6379",
+		"password":                     "foobar",
+		"host":                         "host",
+		"hostname":                     "host",
+		"current_redis_engine_version": "5",
+		"port":                         "6379",
+	}
+
+	if diff := deep.Equal(credentials, expectedCredentials); diff != nil {
 		t.Error(diff)
 	}
 }
