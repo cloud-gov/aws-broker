32cb3a90d6e1267b81f4ef3af5b79ca2c2b375fc
Mark Boyd <mark.boyd@gsa.gov>
2025-10-10 12:14:57 -0400

    address linting issues

--- services/rds/broker.go
+++ services/rds/broker.go
@@ -47,15 +47,15 @@ func (o Options) Validate(settings *config.Settings) error {
 	// Check to make sure that the allocated storage is less than the maximum
 	// allowed.  If allocated storage is passed in, the value defaults to 0.
 	if o.AllocatedStorage > settings.MaxAllocatedStorage {
-		return fmt.Errorf("Invalid storage %d; must be <= %d", o.AllocatedStorage, settings.MaxAllocatedStorage)
+		return fmt.Errorf("invalid storage %d; must be <= %d", o.AllocatedStorage, settings.MaxAllocatedStorage)
 	}
 
 	if o.BackupRetentionPeriod != nil && *o.BackupRetentionPeriod > settings.MaxBackupRetention {
-		return fmt.Errorf("Invalid Retention Period %d; must be <= %d", o.BackupRetentionPeriod, settings.MaxBackupRetention)
+		return fmt.Errorf("invalid Retention Period %d; must be <= %d", o.BackupRetentionPeriod, settings.MaxBackupRetention)
 	}
 
 	if o.BackupRetentionPeriod != nil && *o.BackupRetentionPeriod < settings.MinBackupRetention {
-		return fmt.Errorf("Invalid Retention Period %d; must be => %d", o.BackupRetentionPeriod, settings.MinBackupRetention)
+		return fmt.Errorf("invalid Retention Period %d; must be => %d", o.BackupRetentionPeriod, settings.MinBackupRetention)
 	}
 
 	if err := validateBinaryLogFormat(o.BinaryLogFormat); err != nil {
