26c079f4e65a744763d9aab1a6b7c894f158b092
Mark Boyd <mark.boyd@gsa.gov>
2026-01-16 14:34:59 -0500

    log wait attempts on update

--- services/rds/rds.go
+++ services/rds/rds.go
@@ -250,6 +250,7 @@ func (d *dedicatedDBAdapter) waitForDbReady(operation base.Operation, i *RDSInst
 	// Create a waiter
 	waiter := rds.NewDBInstanceAvailableWaiter(d.rds, func(dawo *rds.DBInstanceAvailableWaiterOptions) {
 		dawo.MinDelay = d.settings.PollAwsMinDelay
+		dawo.LogWaitAttempts = true
 	})
 
 	// Define the waiting strategy
