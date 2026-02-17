816d9d2505368757990cd5b660872fa685d6db01
Mark Boyd <mark.boyd@gsa.gov>
2026-02-17 12:57:07 -0500

    add CI acceptance tests for updating elasticache service plan

---
+++ ci/run-smoke-tests-update-redis.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+. aws-broker-app/ci/ci-utils.sh
+
+# Log in to CF
+login
+
+TEST_ID="$RANDOM"
+SERVICE_NAME="rds-smoke-tests-db-update-$SERVICE_PLAN-$TEST_ID"
+
+# Clean up existing app and service if present
+cf delete -f "smoke-tests-db-update-$SERVICE_PLAN"
+cf delete-service -f "$SERVICE_NAME"
+
+# Create service
+cf create-service aws-elasticache $SERVICE_PLAN $SERVICE_NAME -b "$BROKER_NAME"
+
+# Wait for service to be created
+wait_for_service_instance $TEST_SERVICE
+
+# Update service
+cf update-service "$SERVICE_NAME" -p "$NEW_SERVICE_PLAN"
+
+# Wait to make sure that the service instance has been successfully updated.
+wait_for_service_instance "$SERVICE_NAME"
+
+# Clean up app and service
+cf delete-service -f $TEST_SERVICE
+
+# Wait for service to be deleted
+wait_for_deletion "$SERVICE_NAME"
