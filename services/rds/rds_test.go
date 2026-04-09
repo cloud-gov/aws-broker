1d15f9aaa49c65ad22cf338e77e20db6a9d8a3dc
Mark Boyd <mark.boyd@gsa.gov>
2026-04-09 17:07:58 -0400

    remove unnecessary transaction management

--- services/rds/rds_test.go
+++ services/rds/rds_test.go
@@ -1977,13 +1977,10 @@ func TestBindDBToApp(t *testing.T) {
 
 	for name, test := range testCases {
 		t.Run(name, func(t *testing.T) {
-			// tx := brokerDB.Begin()
-			// err := tx.Create(test.rdsInstance).Error
-			// if err != nil {
-			// 	tx.Rollback()
-			// 	t.Fatal(err)
-			// }
-			// tx.Commit()
+			err := brokerDB.Create(test.rdsInstance).Error
+			if err != nil {
+				t.Fatal(err)
+			}
 
 			creds, err := test.dbAdapter.bindDBToApp(test.rdsInstance, test.password)
 			if err != nil && !test.expectErr {
@@ -2003,11 +2000,11 @@ func TestBindDBToApp(t *testing.T) {
 				t.Error(diff)
 			}
 
-			// dbInstanceRecord := RDSInstance{}
-			// brokerDB.Where("uuid = ?", test.rdsInstance.Uuid).First(&dbInstanceRecord)
-			// if diff := deep.Equal(&dbInstanceRecord, test.expectedInstance); diff != nil {
-			// 	t.Error(diff)
-			// }
+			dbInstanceRecord := RDSInstance{}
+			brokerDB.Where("uuid = ?", test.rdsInstance.Uuid).First(&dbInstanceRecord)
+			if diff := deep.Equal(&dbInstanceRecord, test.expectedInstance); diff != nil {
+				t.Error(diff)
+			}
 		})
 	}
 }
