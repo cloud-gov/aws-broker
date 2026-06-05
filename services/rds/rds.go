14dbdada18f8ac830acffa0a67ca7129e22a17d3
Mark Boyd <mark.boyd@gsa.gov>
2026-06-05 15:45:15 -0400

    reconcile allocated storage on instance with actual value from RDS, if necessary (#507)

--- services/rds/rds.go
+++ services/rds/rds.go
@@ -352,6 +352,11 @@ func (d *dedicatedDBAdapter) reconcileDbState(ctx context.Context, i RDSInstance
 		reconciledInstance.DbVersion = *dbInstanceState.EngineVersion
 	}
 
+	// reconcile storage with actual instance storage, if necessary
+	if reconciledInstance.AllocatedStorage != int64(*dbInstanceState.AllocatedStorage) {
+		reconciledInstance.AllocatedStorage = int64(*dbInstanceState.AllocatedStorage)
+	}
+
 	return &reconciledInstance, nil
 }
 
