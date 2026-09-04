656be1ba93b8ff4702061b41cadc40df4d78c62c
Mark Boyd <mark.boyd@gsa.gov>
2026-09-04 16:53:35 -0400

    add smoke test for updating database using custom parameter group to a new version

--- ci/run-smoke-tests-db-updates.sh
+++ ci/run-smoke-tests-db-updates.sh
@@ -2,6 +2,7 @@
 
 set -euxo pipefail
 
+# shellcheck disable=SC1091
 . aws-broker-app/ci/ci-utils.sh
 
 # Log in to CF
@@ -41,6 +42,10 @@ fi
 
 if [ -n "$ENABLE_CLOUDWATCH_LOG_GROUP_EXPORTS_ON_CREATE" ]; then
   create_service_args+=(-c "{\"enable_cloudwatch_log_groups_exports\": $ENABLE_CLOUDWATCH_LOG_GROUP_EXPORTS_ON_CREATE}")
+fi
+
+if [ -n "$ENABLE_PG_CRON_ON_CREATE" ]; then
+  create_service_args+=(-c "{\"enable_pg_cron\": true}")
 fi
 
 cf create-service "${create_service_args[@]}"
