b367f9ed4459843e057856adc6163483d9c4f61d
Jason Gambino <Jason.gambino@gsa.gov>
2026-09-17 16:15:13 -0400

    Update services/redis/modify_worker_test.go
    Co-authored-by: Mark Boyd <mark.boyd@gsa.gov>
    Signed-off-by: Jason Gambino <jason.gambino3@gmail.com>

--- services/redis/modify_worker_test.go
+++ services/redis/modify_worker_test.go
@@ -233,7 +233,7 @@ func TestAsyncModifyRedis(t *testing.T) {
 				brokerDB,
 				&config.Settings{
 					PollAwsMinDelay:    1 * time.Millisecond,
-					PollAwsMaxDuration: 30 * time.Second,
+					PollAwsMaxDuration: 10 * time.Second,
 				},
 				&mockRedisClient{
 					describeReplicationGroupsResults: []*elasticache.DescribeReplicationGroupsOutput{
